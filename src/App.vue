8b71e85d2b0893c0beeecae44ed431e5ab18203e
Filippo Comelato <filippocomelato19@gmail.com>
2025-10-30 18:47:07 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref, onMounted } from "vue";
+import gsap from "gsap";
+import { ScrollTrigger, SplitText } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+</script>
+
+<template>
+  <div class="flex-center h-screen">Hello World</div>
+</template>
+
+<style scoped></style>
